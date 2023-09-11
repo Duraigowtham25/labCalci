83a53929d44d70d37dab931177eb6e48c08c85dc
Gowtham D <119847172+Duraigowtham25@users.noreply.github.com>
2023-09-11 11:41:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/calculator.html');
+});
+
+app.post('/calculate', (req, res) => {
+    const num1 = parseFloat(req.body.num1);
+    const num2 = parseFloat(req.body.num2);
+    const operation = req.body.operation;
+
+    let result;
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'sub':
+            result = num1 - num2;
+            break;
+        case 'mul':
+            result = num1 * num2;
+            break;
+        case 'div':
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = 'Cannot divide by zero';
+            }
+            break;
+        default:
+            result = 'Invalid operation';
+    }
+
+    res.send(`Result: ${result}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
